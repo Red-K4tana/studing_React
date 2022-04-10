7044bb6208eab1065aa8380fae430943f2ae8fdc
Red-K4tana <ceptor.nill@gmail.com>
2022-04-10 22:59:47 +0300

    AppWithRedux

---
+++ src/api/todolist-api.ts
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const instance = axios.create({
+    baseURL: 'https://social-network.samuraijs.com/api/1.1/',
+    withCredentials: true,
+    headers: {
+        'API-KEY': '9bad4dc1-4f5c-485a-93a2-c73a0c7180c8'
+    }
+})
+
+// API
+export const todolistAPI = {
+
+}
